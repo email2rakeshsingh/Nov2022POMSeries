3ef4f98b7cb5e9216d8455de21dd6ab48ebdf0f9
email2rakeshsingh <email2rakeshsingh@gmail.com>
2023-04-06 17:17:16 +0530

    code has updated

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,8 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
+
+	private By cart = By.id("page-cart");
+
 	public void cartPageRakesh() {
+		
 		System.out.println("working on cart pages");
+		System.out.println("Click on :+ Cart");
 	}
 
 }
