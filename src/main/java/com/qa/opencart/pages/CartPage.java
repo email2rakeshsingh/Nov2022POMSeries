8f7b70b570529dcd5f02976e0e25ec3143f7238a
email2rakeshsingh <email2rakeshsingh@gmail.com>
2023-04-10 10:41:08 +0530

    added cart page code as per dev team comment

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -6,6 +6,7 @@ public class CartPage {
 
 	private By cart = By.id("page-cart");
 	private By cart1 = By.id("page item");
+	private By cart2 = By.xpath("Rakesh.singh//rws");
 
 	public void cartPageRakesh() {
 		
